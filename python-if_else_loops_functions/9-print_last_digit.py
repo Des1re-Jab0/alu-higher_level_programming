dcb040e93d4c9819412b5e9b24b2085d7f3c5732
= <d.manzijabo@alustudent.com>
2026-06-26 08:31:01 +0200

    Error Fixed

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        print("{}".format(chr(ord(c) - 32 * (96 < ord(c) < 123))), end="")
-    print("")
+def print_last_digit(number):
+    last = abs(number) % 10
+    print("{}".format(last), end="")
+    return last
