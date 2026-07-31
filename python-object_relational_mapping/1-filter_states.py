61f38c29585b8ab6b37d056d13f9d8fc6d865f2f
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 15:26:39 -0700

    Fix Tasks 1 and 3: restore %s placeholders dropped by printf escaping

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE  ORDER BY id ASC",
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                 ("N%",))
     for row in cur.fetchall():
         print(row)
