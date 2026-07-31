1ef0a24d096d4b7c39db0a18353076aee0e5f752
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 15:32:53 -0700

    Fix Task 2: use BINARY for case-sensitive equality match

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(
+        sys.argv[4])
     cur.execute(query)
     for row in cur.fetchall():
         print(row)
