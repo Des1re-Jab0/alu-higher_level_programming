4d92c15721050de86fa216a5a37150f2f7659afb
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 15:30:21 -0700

    Fix Task 1: use BINARY for case-sensitive LIKE match on uppercase N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                ("N%",))
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC",
+        ("N%",))
     for row in cur.fetchall():
         print(row)
     cur.close()
