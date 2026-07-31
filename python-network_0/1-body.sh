9c10fbf67c7cf25a810260eb2a1b87d3c21ecdc2
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-30 22:59:12 -0700

    1-body.sh error fixed

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request and displays the body only when the status code is 200
-[ "$(curl -s -o /tmp/1-body_output -w "%{http_code}" "$1")" = "200" ] && cat /tmp/1-body_output
+# sends a GET request, follows any redirects, and displays the body only if the final status is 200
+[ "$(curl -s -L -o /tmp/1-body_output -w "%{http_code}" "$1")" = "200" ] && cat /tmp/1-body_output
