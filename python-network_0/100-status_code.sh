5f2238c389702f938fd4dc89a870a8dbde572d83
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 00:12:37 -0700

    100-status_code.sh fixed

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays only the HTTP status code of the response, no pipes or redirection
-curl -s -o /dev/null -w "%{http_code}\n" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
