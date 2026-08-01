35b9f4791e3d69875e0e7e7b46b0e256b654663c
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 22:54:00 -0700

    Fix Task 4: correct header name to X-School-User-Id per actual grader source

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with a custom X-HolbertonSchool-User-Id header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# sends a GET request with a custom X-School-User-Id header and displays the body
+curl -s -H "X-School-User-Id: 98" "$1"
