1e630ff4ceb1e1f05bee9c7dfbec7d353d03622c
Des1re-Jab0 <d.manzijabo@alustudent.com>
2026-07-31 22:22:00 -0700

    Fix Task 8 PEP8: correct continuation line indentation for E127

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     letter = ""
     if len(sys.argv) > 1:
         letter = sys.argv[1]
-    response = requests.post("http://0.0.0.0:5000/search_user",
-                              data={"q": letter})
+    response = requests.post(
+        "http://0.0.0.0:5000/search_user", data={"q": letter})
     try:
         json_body = response.json()
     except ValueError:
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         if not json_body:
             print("No result")
         else:
-            print("[{}] {}".format(json_body.get("id"),
-                                    json_body.get("name")))
+            print("[{}] {}".format(
+                json_body.get("id"), json_body.get("name")))
